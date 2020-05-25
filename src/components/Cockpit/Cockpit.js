a9041f49b7ace53ec39745c6657ed66a23ddd99e
Renan Zulian <renanzulian@gmail.com>
2020-05-25 17:07:07 -0300

    C94 - useEffect implementation

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const cockpit = (props) => {
   console.log('[Cockpit] renderirng...')
+  useEffect(() => {
+    console.log('[Cockpit] useEffect')
+    // Here we can post some data to api for example
+  })
   return (
     <div>
       <h1>{props.title}</h1>
